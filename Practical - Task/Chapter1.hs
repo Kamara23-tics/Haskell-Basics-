3ac72b560542de091070648844f42b80ba14e481
Kamara23-tics <graceonah79@gmail.com>
2025-09-12 21:15:04 +0100

    Update Chapter1.hs

--- Practical - Task/Chapter1.hs
+++ Practical - Task/Chapter1.hs
@@ -7,3 +7,25 @@ main = do
  print $ double 4
 
  -- answer = 8
+
+
+--increment: Increases a number by 1.
+increment :: Int -> Int
+increment x = x + 1
+
+main :: IO ()
+main = do
+ print $ increment 5
+
+-- answer = 6
+
+
+--doubleThenIncrement Uses function composition to apply double first and then increment
+doubleThenincrement :: Int -> Int
+doubleThenincrement x = x * 2 + 1
+
+main :: IO ()
+main = do
+ print $ doubleThenincrement 5
+
+ --answer = 11
