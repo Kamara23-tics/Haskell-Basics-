62b7b01aa9446f85abb53a6ac079d5e1a554f1dc
Kamara23-tics <graceonah79@gmail.com>
2025-09-17 17:00:49 +0100

    Update Chapter2.hs

--- Practical - Task/Chapter2.hs
+++ Practical - Task/Chapter2.hs
@@ -5,3 +5,14 @@
 "Haskell" -> String 
 'Z' -> Char
 True && False -> Boolean 
+
+--Task 2: Function Type Signatures
+--A function add that takes two Int values and returns their sum
+add :: Int -> Int -> Int
+add a b  = a + b
+
+main :: IO ()
+main = do
+ print $ add 4 5
+ --Output 
+ 9
