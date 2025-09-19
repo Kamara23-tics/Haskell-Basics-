fa723471aa92460fd497eda3752f50cf932a096f
Kamara23-tics <graceonah79@gmail.com>
2025-09-19 10:58:32 +0100

    Update Chapter2.hs

--- Practical - Task/Chapter2.hs
+++ Practical - Task/Chapter2.hs
@@ -14,15 +14,30 @@ add a b  = a + b
 main :: IO ()
 main = do
  print $ add 4 5
+ 
  --Output 
  9
 
 
+--A function isEven that takes an Int and returns a Bool indicating if it's even
 isEven :: Int -> Bool
 isEven y = y `mod` 2 == 0
 
 main :: IO ()
 main = do
  print $ isEven 11
+ 
  --Output
  False 
+
+ 
+--A function concatStrings that takes two String values and returns their concatenation
+concatStrings :: String -> String -> String
+concatStrings x y =  x ++ " " ++ y
+
+main :: IO ()
+main = do
+ print $ concatStrings "Merry" "Christmas"
+
+-- Output
+"Merry Christmas"
