2411cb4dfb3f84db14fcb3bae1d5f07f57b0d593
Kamara23-tics <graceonah79@gmail.com>
2025-09-30 14:04:17 +0100

    Update Chapter3.hs

--- Practical - Task/Chapter3.hs
+++ Practical - Task/Chapter3.hs
@@ -1,1 +1,20 @@
+--Task 1
+checkNumber :: Int -> String 
+checkNumber y =
+  if y > 0
+    then "positive"
+     else if y < 0
+      then "negative"
+       else "neutral"
+        
+main :: IO ()
+main = do
+ putStrLn $ checkNumber 5
+ putStrLn $ checkNumber (-3)
+ putStrLn $ checkNumber 0
 
+ --Output
+ "positive"
+ "negative"
+ "neutral"
+ 
