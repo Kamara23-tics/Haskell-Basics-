493014e6c2a208fa89d31a5d0cc1183b190faa57
Kamara23-tics <graceonah79@gmail.com>
2025-10-04 07:52:16 +0100

    Update Chapter3.hs

--- Practical - Task/Chapter3.hs
+++ Practical - Task/Chapter3.hs
@@ -92,3 +92,24 @@ main = do
  "Equilateral" 
  "Isosceles" 
  "Scalene"
+ --Task 6
+ --Check leap year using if-then-else
+isLeapYear :: Int -> Bool
+isLeapYear x =
+ if x `mod` 400 == 0
+   then True
+     else if x `mod` 100 == 0 || x `mod` 400 == 0
+       then  False
+         else if x `mod` 4 == 0 
+           then True
+             else False
+
+main :: IO ()
+main = do
+ print $ isLeapYear 2000
+ print $ isLeapYear 1900
+ print $ isLeapYear 2024
+ --Output
+ True
+ False
+ True
