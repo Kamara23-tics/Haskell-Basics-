2db3e3a63f3bd87cebc738e77007433acb25c7ac
Kamara23-tics <graceonah79@gmail.com>
2025-10-21 10:56:01 +0100

    Update Chapter5.hs

--- Chapter5.hs
+++ Chapter5.hs
@@ -10,3 +10,11 @@ main = do
  print $ applyThrice double 4
  --Output
  32
+filterOddNumbers :: [Int] -> [Int]
+filterOddNumbers a = filter odd a
+
+main :: IO ()
+main = do
+ print $ filterOddNumbers  [1..30]
+ --Output:
+[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29]
