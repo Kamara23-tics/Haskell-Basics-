89dff23498fb5ad6f52dfa50012daf241b7a84c3
Kamara23-tics <graceonah79@gmail.com>
2025-11-14 15:12:23 +0100

    Update Chapter6.hs

--- Chapter6.hs
+++ Chapter6.hs
@@ -1,3 +1,4 @@
+--Task 1 Factorial (Recursive)
 factorial :: Int -> Int
 factorial 0 = 1 
 factorial x = x * factorial (x-1)
@@ -8,3 +9,15 @@ main = do
 
 --Output
 5040
+
+--Task 2 Fibonacci (Recursive)
+nthFibonacci :: Int -> Int
+nthFibonacci 0 = 0
+nthFibonacci 1 = 1 
+nthFibonacci n = nthFibonacci(n-1) + nthFibonacci(n-2)
+
+main :: IO ()
+main = do
+ print $ nthFibonacci 8
+ --Output 
+ 21
