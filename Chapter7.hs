c7966561ae3efc96d3a8e584c8d3dcc33af2ad1a
Kamara23-tics <graceonah79@gmail.com>
2025-12-04 05:45:03 +0100

    Update Chapter7.hs

--- Chapter7.hs
+++ Chapter7.hs
@@ -19,3 +19,30 @@ Testing Eq instance for Color:
 True
 False
 True
+--Task 2:Using the Color type from HC7T1, implement the Ord type class so that Red < Green < Blue.
+data Color = Red | Green | Blue
+
+instance Eq Color where
+  Red   == Red   = True
+  Green == Green = True
+  Blue  == Blue  = True
+  _     == _     = False
+  
+instance Ord Color where
+  compare Red Red     = EQ
+  compare Green Green = EQ
+  compare Blue Blue   = EQ
+  compare Red _       = LT      
+  compare _ Red       = GT  
+  compare Green Blue  = LT      
+  compare Blue Green  = GT      
+  
+main :: IO ()
+main = do
+  putStrLn "Testing Eq instance for Color:"
+  print $ Red == Red
+  print $ Red == Green
+  --Output:
+Testing Eq instance for Color:
+True
+False
