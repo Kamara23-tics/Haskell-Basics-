591ad2112f32248fcee26e3da2c81c1a15fdf71d
Kamara23-tics <graceonah79@gmail.com>
2025-12-04 05:30:01 +0100

    Update Chapter7.hs

--- Chapter7.hs
+++ Chapter7.hs
@@ -1,1 +1,21 @@
+--Task 1: Implement an Eq Instance for a Custom Data Type
+Define a data type Color representing Red, Green, and Blue.
+data Color = Red | Green | Blue
 
+instance Eq Color where
+  Red   == Red   = True
+  Green == Green = True
+  Blue  == Blue  = True
+  _     == _     = False
+
+main :: IO ()
+main = do
+  putStrLn "Testing Eq instance for Color:"
+  print $ Red == Red
+  print $ Red == Green
+  print $ Blue == Blue
+  --Output:
+Testing Eq instance for Color:
+True
+False
+True
