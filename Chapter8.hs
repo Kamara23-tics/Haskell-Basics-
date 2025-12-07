39ad9ef951d916a062a307edd235256738243a06
Kamara23-tics <graceonah79@gmail.com>
2025-12-07 20:16:24 +0100

    Update Chapter8.hs

--- Chapter8.hs
+++ Chapter8.hs
@@ -1,1 +1,12 @@
 
+type Address = String
+type Value = Int
+
+generateTx :: Address -> Address -> Value -> String
+generateTx from to amount = from ++ " " ++ to ++ " " ++ show amount
+
+main :: IO ()
+main = do
+    putStrLn $ generateTx "addr1" "addr2" 100
+    putStrLn $ generateTx "wallet_A" "wallet_B" 500
+    putStrLn $ generateTx "0x123abc" "0x456def" 250
