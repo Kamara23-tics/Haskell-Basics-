63ec1078f6eef2fd2117de1563f598e33413375c
Kamara23-tics <graceonah79@gmail.com>
2025-12-07 20:27:13 +0100

    Update Chapter8.hs

--- Chapter8.hs
+++ Chapter8.hs
@@ -14,3 +14,43 @@ main = do
 addr1 addr2 100
 wallet_A wallet_B 500
 0x123abc 0x456def 250
+
+--Task 2:
+type Address = String
+type Value = Int
+
+generateTx :: Address -> Address -> Value -> String
+generateTx from to amount = from ++ " " ++ to ++ " " ++ show amount
+
+data PaymentMethod = Cash | Card | Cryptocurrency
+    deriving (Show, Eq)
+
+data Person = Person
+    { name :: String
+    , address :: (String, Int)
+    , paymentMethod :: PaymentMethod
+    } deriving (Show, Eq)
+
+bob :: Person
+bob = Person
+    { name = "Bob"
+    , address = ("123 Main St", 12345)
+    , paymentMethod = Cash
+    }
+
+main :: IO ()
+main = do
+    putStrLn "Transaction examples:"
+    putStrLn $ generateTx "addr1" "addr2" 100
+    putStrLn $ generateTx "wallet_A" "wallet_B" 500
+    putStrLn ""
+    putStrLn "Person example:"
+    print bob
+--Output:
+Transaction examples:
+addr1 addr2 100
+wallet_A wallet_B 500
+
+Person example:
+Person {name = "Bob", address = ("123 Main St",12345), paymentMethod = Cash}
+
