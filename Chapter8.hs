61d61abacbdf6b23cef3fa1687aa22e0072072a7
Kamara23-tics <graceonah79@gmail.com>
2025-12-07 20:20:11 +0100

    Update Chapter8.hs

--- Chapter8.hs
+++ Chapter8.hs
@@ -1,4 +1,4 @@
-
+--Task 1: Type Synonyms and Basic Function
 type Address = String
 type Value = Int
 
@@ -10,3 +10,7 @@ main = do
     putStrLn $ generateTx "addr1" "addr2" 100
     putStrLn $ generateTx "wallet_A" "wallet_B" 500
     putStrLn $ generateTx "0x123abc" "0x456def" 250
+--Output:
+addr1 addr2 100
+wallet_A wallet_B 500
+0x123abc 0x456def 250
